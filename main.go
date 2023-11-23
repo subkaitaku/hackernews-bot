b51c7a88ac7371a61972ff32cee0088968c5265a
Shiba <subikuho@gmail.com>
2023-11-23 16:10:35 +0900

    until-get-news

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -16,6 +19,57 @@ func main() {
 	}
 }
 
+type ItemDetail struct {
+	Title string `json:"title"`
+	URL   string `json:"url"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, Go Server!")
+	const topStoriesURL = "https://hacker-news.firebaseio.com/v0/topstories.json"
+	stRes, err := http.Get(topStoriesURL)
+	if err != nil {
+		fmt.Println("error!!", err)
+		return
+	}
+	defer stRes.Body.Close()
+
+	body, err := io.ReadAll(stRes.Body)
+	if err != nil {
+		fmt.Println("body parse error!!: ", err)
+		return
+	}
+	var storyNums []int
+	err = json.Unmarshal([]byte(body), &storyNums)
+	if err != nil {
+		fmt.Println("error!!", err)
+		return
+	}
+
+	var result []map[string]string
+	for _, value := range storyNums[:10] {
+		detailURl := "https://hacker-news.firebaseio.com/v0/item/" + strconv.Itoa(value) + ".json"
+		detailRes, err := http.Get(detailURl)
+		if err != nil {
+			fmt.Println("error!!", err)
+			return
+		}
+		defer detailRes.Body.Close()
+
+		detailBody, err := io.ReadAll(detailRes.Body)
+		if err != nil {
+			fmt.Println("body parse error!!: ", err)
+			return
+		}
+
+		var itemDetail ItemDetail
+		err = json.Unmarshal(detailBody, &itemDetail)
+		if err != nil {
+			fmt.Println("json parse error!: ", err)
+			return
+		}
+		result = append(result, map[string]string{"title": itemDetail.Title, "url": itemDetail.URL})
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
 }
